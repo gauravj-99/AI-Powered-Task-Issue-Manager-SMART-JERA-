5b091ff4569dc3ef9130650bb1037744105b4c32
Gaurav Joshi <ggauravjoshi99@gmail.com>
2026-07-22 11:17:27 +0530

    start Frontend

---
+++ Frontend/src/pages/register.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+
+function Register() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "member",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await api.post("/auth/register", formData);
+
+      alert("Registration Successful");
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      alert("Registration Failed");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+
+      <br />
+
+      <Link to="/">
+        Already have an account? Login
+      </Link>
+    </div>
+  );
+}
+
+export default Register;
