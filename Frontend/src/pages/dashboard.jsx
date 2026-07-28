ae3ac8b717507ccc4da0280321fe845ff2a7d168
Gaurav Joshi <ggauravjoshi99@gmail.com>
2026-07-28 13:05:42 +0530

    fronted CRUD

--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -1,14 +1,18 @@
 import api from "../services/api";
-import{ useState} from "react";
+import {useState, useEffect } from "react";
+
 import {useNavigate} from "react-router-dom";
 function Dashboard(){
     const [title, setTitle]=useState("");
     const [description, setDescription]=useState("");
+    const [projects, setProjects]= useState([]);
     const navigate=useNavigate();
     const token = localStorage.getItem("token");
+    useEffect(()=>{
+        fetchProjects();
+    }, []);
     const createProject=async()=>{
     try{
-        const token =localStorage.getItem("token");
         await api.post(
             "/projects",
             {
@@ -24,33 +28,58 @@ function Dashboard(){
             }
         );
         alert("Project Created");
+        fetchProjects();
+        setTitle("");
+        setDescription("");
 
     }catch(error){
         console.log(error);
+        alert("project not created")
     }
 };
-<button onClick={createProject}>
-    Create Project
-</button>
     const logoutHandler=()=>{
         localStorage.removeItem("token");
         navigate("/");
+    };
+    
+    const fetchProjects = async ()=>{
+        try{
+            const {data}=await api.get("/projects",{
+                headers:{
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setProjects(data);
+        }catch(error){
+            console.log(error);
+        }
+    };
 
+    const deleteProject=async (id)=>{
+        try{
+            await api.delete(
+                `/projects/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            alert("Project Deleted");
+            fetchProjects();
+        }catch(error){
+            console.log(error);
+        }
     };
     return(
         <div>
             <h1>Dashboard</h1>
             <h3>Login Successfully</h3>
-            <p>{token}</p>
-            <textarea
-            rows="8"
-            cols="80"
-            value={token}
-            readOnly
-            />
-
-            <br/>
-            <br/>
+            <button onClick={logoutHandler}>
+                logout
+            </button>
+            <hr/>
+            <h2>Create Project</h2>
 
             <input 
             type="text"
@@ -71,20 +100,32 @@ function Dashboard(){
                 setDescription(e.target.value)
             }
             />
-            <button onClick={logoutHandler}>
-                logout
+
+            <br/>
+            <br/>
+            <button onClick={createProject}>
+                Create Project
             </button>
+            <hr />
 
-            <hr/>
-            <hr/>
+            <h2>Projects</h2>
+            {projects.map((project)=>(
+                <div key={project._id}>
+                    <h3>{project.title}</h3>
+                    <p>{project.description}</p>
+                    <button onClick={()=>
+                        deleteProject(project._id)}>
+                        Delete
+                    </button>
+                    <hr />
+                </div>
 
-            <h2>
-                Projects
-            </h2>
-            <button>Create Project</button>
+            ))}         
         </div>
 
     );
+   
+    
 }
 
 export default Dashboard;
