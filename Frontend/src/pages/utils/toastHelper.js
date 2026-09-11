7ee19fb24fc1d35604a86e8832b4ffd05671f653
Gaurav Joshi <ggauravjoshi99@gmail.com>
2026-09-11 21:38:07 +0530

    Use Alert in on folder

--- Frontend/src/pages/utils/toastHelper.js
+++ Frontend/src/pages/utils/toastHelper.js
@@ -7,3 +7,11 @@ export const showSuccess = (message) => {
 export const showError = (message) => {
   toast.error(message);
 };
+
+export const showInfo = (message) => {
+  toast.info(message);
+};
+
+export const showWarning = (message) => {
+  toast.warning(message);
+};
